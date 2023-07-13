fd1fe84291a0d643df6d3ea220d51def5d8675e1
roganOhDev <fdscbjdcnhd@gmail.com>
2023-07-13 00:25:26 -0700

    delete build folder

--- main/build/generated/source/kaptKotlin/main/Handler/testController2.kt
+++
@@ -1,3 +0,0 @@
-package Handler
-
-class TestController2
--- main/build/tmp/kapt3/stubs/main/HandlerBridgeKt.java
+++
@@ -1,7 +0,0 @@
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 7, 1}, k = 2, d1 = {"\u0000\u0006\n\u0000\n\u0002\u0018\u0002\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0082\u0004\u00a2\u0006\u0002\n\u0000"}, d2 = {"rootHandler", "LRootHandler;"})
-public final class HandlerBridgeKt {
-    private static final RootHandler rootHandler = null;
-}
